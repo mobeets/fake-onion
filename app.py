81a0eb37cd4630ab0f7417d47f1172dec34d0541
mobeets <mobeets@gmail.com>
2016-07-19 14:00:13 -0400

    app

---
+++ app.py
@@ -0,0 +1,53 @@
+import os
+import time
+from random import random
+from twython import Twython
+import headlines
+
+CONSUMER_KEY = os.environ['TWITTER_CONSUMER_KEY']
+CONSUMER_SECRET = os.environ['TWITTER_CONSUMER_SECRET']
+OAUTH_TOKEN = os.environ['TWITTER_OAUTH_TOKEN']
+OAUTH_TOKEN_SECRET = os.environ['TWITTER_OAUTH_TOKEN_SECRET']
+TWEET_LENGTH = 140
+TWEET_URL_LENGTH = 21
+USERNAME = 'DicedOnionBot'
+
+RUN_EVERY_N_SECONDS = 60*5 # e.g. 60*5 = tweets every five minutes
+
+def twitter_handle():
+    return Twython(CONSUMER_KEY, CONSUMER_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+
+def submit_tweet(message, handle=None):
+    if not handle:
+        handle = twitter_handle()
+    handle.update_status(status=message)
+
+def list_of_tweets(handle, count=200, username=USERNAME):
+    user_timeline = handle.get_user_timeline(screen_name=username, \
+        count=count)
+    return [tweet['text'] for tweet in user_timeline]
+
+def get_message(mdl, corpus, handle):
+    """
+    Generate message from Markov model
+    """
+    said_before = True
+    while said_before:
+        msg = headlines.get_msg(mdl, max_length=TWEET_LENGTH, \
+            text=corpus)
+        # check if it's been said already
+        said_before = msg in list_of_tweets(handle)
+    assert len(msg) <= TWEET_LENGTH
+    return msg
+
+def main():
+    handle = twitter_handle()
+    mdl, corpus = headlines.get_model(infile=headlines.DATAFILE_ONION)
+    while True:
+        message = get_message(mdl, corpus, handle)
+        print message
+        submit_tweet(message, handle)
+        time.sleep(RUN_EVERY_N_SECONDS)
+
+if __name__ == '__main__':
+    main()
--- headlines.py
+++ headlines.py
@@ -15,31 +15,36 @@ def load(infile):
     with open(infile) as f:
         return f.read()
 
-def generate(mdl, n, text=''):
-    c = 0
-    while c < n:
-        sent = mdl.make_short_sentence(140, tries=1000, \
-            max_overlap_total=6, max_overlap_ratio=0.5)
-        if sent is not None and sent not in text:
-            c += 1
-            print sent
+def get_msg(mdl, max_length=140, max_tries=1000, \
+    max_overlap_total=6, max_overlap_ratio=0.5, text=''):
+    
+    msg = None
+    while msg is None or msg in text:
+        msg = mdl.make_short_sentence(max_length, tries=max_tries, \
+            max_overlap_total=max_overlap_total, \
+            max_overlap_ratio=max_overlap_ratio)
+    return msg
+
+def get_model(infile=DATAFILE_ONION):
+    text = load(infile)
+    return markovify.NewlineText(text, state_size=2), text
 
 def markov(infile=DATAFILE_ONION, n=20):
-    text = load(infile)
-    text_model = markovify.NewlineText(text, state_size=2)
-    generate(text_model, n, text)
+    text_model, text = get_model(infile)
+    for i in xrange(n):
+        print get_msg(text_model, text=text)
 
 def combine(infiles=[DATAFILE_DOI, DATAFILE_ONION], n=20):
     ts = []
     mdls = []
     for infile in infiles:
-        text = load(infile)
+        mdl, text = get_model(infile)
         ts.append(text)
-        mdl = markovify.NewlineText(text, state_size=2)
         mdls.append(mdl)
 
     M = markovify.combine(mdls, weights=[1.0, 0.4])
-    generate(M, n, '\n'.join(ts))
+    for i in xrange(n):
+        print get_msg(M, text='\n'.join(ts))
 
 if __name__ == '__main__':
     import sys
